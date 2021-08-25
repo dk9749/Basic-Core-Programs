14ceab3eae392aec3b8f8888bda6b118628f7ee2
unknown <mr.dineshdkd@gmail.com>
2021-08-25 20:25:46 +0530

    Added: Swapping Two Numbers

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.bridgelab;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 11, b = 12;
+        int c;
+        System.out.println(" a = " + a + " b = " + b);
+
+        c = a;
+        a = b;
+        b = c;
+        System.out.println(" a= " + a + " b= " + b);
+
+    }
+}
